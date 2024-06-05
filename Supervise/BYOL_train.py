ac6bf526e17f514322f6d0b5f1e00828147aeb28
mango7789 <981653507@qq.com>
2024-06-05 17:02:28 +0800

    [FIX]: some variable and value in train parser of Supervise

--- Supervise/BYOL_train.py
+++ Supervise/BYOL_train.py
@@ -9,8 +9,8 @@ def BYOL_train():
     parser.add_argument('--output_dim', type=int, default=256, help='Dimension of the prediction space')
     parser.add_argument('--update_rate', type=float, default=0.996, help='Update rate of the target by moving average')
     parser.add_argument('--save', type=bool, default=False, help='Save model flag')
-    parser.add_argument('--seed', type=int, default=603, help='Random seed for reproduceability')
-    parser.add_argument('--data_root', type=str, default='./data/', help='Root directory of the data')
+    parser.add_argument('--seed', type=int, default=605, help='Random seed for reproduceability')
+    parser.add_argument('--root', type=str, default='./data/', help='Root directory of the data')
     parser.add_argument('--batch_size', type=int, default=64, help='Size of each batch during training')
     parser.add_argument('--num_workers', type=int, default=2, help='Number of workers used in data loading')
     parser.add_argument('--lr_configs', type=dict, default={}, help='Additional configs for learning rate')
@@ -25,7 +25,7 @@ def BYOL_train():
         update_rate=args.update_rate,
         save=args.save,
         seed=args.seed,
-        data_root=args.data_root,
+        root=args.data_root,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
         lr_configs=args.lr_configs,
--- Supervise/resnet18_train.py
+++ Supervise/resnet18_train.py
@@ -6,10 +6,10 @@ def resnet_train():
 
     parser.add_argument('--epochs_resnet', type=int, default=30, help='Number of training epochs for ResNet-18')
     parser.add_argument('--lr', type=float, default=0.001, help='Learning rate for training ResNet-18')
-    parser.add_argument('--save_encoder', type=bool, default=False, help='Save the trained ResNet-18 model')
+    parser.add_argument('--save', type=bool, default=False, help='Save the trained ResNet-18 model')
 
-    parser.add_argument('--seed', type=int, default=603, help='Random seed for reproduceability')
-    parser.add_argument('--data_root', type=str, default='./data/', help='Root directory of the data')
+    parser.add_argument('--seed', type=int, default=605, help='Random seed for reproduceability')
+    parser.add_argument('--root', type=str, default='./data/', help='Root directory of the data')
     parser.add_argument('--batch_size', type=int, default=64, help='Size of each batch during training')
     parser.add_argument('--num_workers', type=int, default=2, help='Number of workers used in data loading')
     parser.add_argument('--lr_configs', type=dict, default={}, help='Additional configs for learning rate of ResNet-18')
@@ -19,9 +19,9 @@ def resnet_train():
     train_resnet18(
         epochs=args.epochs_resnet,
         lr=args.lr,
-        save=args.save_encoder,
+        save=args.save,
         seed=args.seed,
-        data_root=args.data_root,
+        data_root=args.root,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
         lr_configs=args.lr_configs,
